2835fe7f0d0988f2171bfb98e9eb3dcd4e589da7
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-03 14:21:03 +0100

    feat: add endpont to get all client

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const customers= require('./customers.json')
 
-app.get('/api/client', (req, res) => {
-  res.send('Salut le monde!')
+//ACCUEIL
+app.get('/', (req, res) => {
+	res.send('<h1>Liste de clients</h1>')
 })
 
+//ALL CLIENT
+app.get('/api/client', (req, res) => {
+  res.status(200).json(customers)
+})
+
+
+//SERVEUR
 app.listen(port, () => {
   console.log(`le serveur tourne au port ${port}`)
 })
